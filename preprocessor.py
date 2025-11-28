19d9aff2f9f126631ae0dddff8ad3b170bf73be6
victorli <ths24108@towerhill.org>
2025-11-27 23:02:25 -0500

    fixed SOH

--- preprocessor.py
+++ preprocessor.py
@@ -173,8 +173,8 @@ def get_discharges_phyiscs(filepath, df):
     """
     df_discharges = df[df['type'] == 'discharge'][['start_time', 'ambient_temperature', 'battery_id', 'uid', 'filename', 'Capacity']].copy()
     df_discharges['cycle_number'] = df_discharges.groupby('battery_id').cumcount() + 1
-    df["C_nominal"] = df["battery_id"].map(C_nominal)
-    df["SOH"] = df["Capacity"] / df["C_nominal"]
+    df_discharges["C_nominal"] = df_discharges["battery_id"].map(C_nominal)
+    df_discharges["SOH"] = df_discharges["Capacity"] / df_discharges["C_nominal"]
     
     mean_voltage = []
     max_voltage = []
